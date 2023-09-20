2c44be1d44c45e7ce39ab978dfe59c5ec06da15c
mangelia <mangelia@cisco.com>
2023-09-19 22:08:35 -0600

    Prep 2 done

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,29 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        Console.WriteLine("What is your grade percentage?");
+        string percentage_by_user = Console.ReadLine();
+        int percentage_to_int = int.Parse(percentage_by_user);
+
+        if (percentage_to_int >= 90)
+        {
+            Console.WriteLine("Your grade is A");
+        }
+        else if (percentage_to_int >= 80)
+        {
+            Console.WriteLine("Your grade is B");
+        }
+        else if (percentage_to_int >= 70)
+        {
+            Console.WriteLine("Your grade is C");
+        }
+        else if (percentage_to_int >= 60)
+        {
+            Console.WriteLine("Your grade is D");
+        }
+        else if (percentage_to_int < 60)
+        {
+            Console.WriteLine("Your grade is F");
+        }
     }
 }
