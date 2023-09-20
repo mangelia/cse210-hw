e592fa840b9d4334d9c704e1228ec5c68816a6e5
mangelia <mangelia@cisco.com>
2023-09-20 16:47:48 -0600

    Prep 3 done

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,6 +4,43 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        
+        Console.WriteLine("What is the magic number? Please type your guess between 1 and 100!");
+        Random randomGenerator = new Random();
+
+        //This section is for the random number
+        int number = randomGenerator.Next(1, 100);
+        int answer_to_int = number;
+        int answer_guess_to_int_while;
+        Console.WriteLine(number);
+
+        /* This section works when the user types the magic number
+        string answer = Console.ReadLine();
+        int answer_to_int = int.Parse(answer);
+        int answer_guess_to_int_while;
+        */
+
+        do
+        {
+        Console.Write("What is your guess?");
+        string answer_guess = Console.ReadLine();
+        int answer_guess_to_int = int.Parse(answer_guess);
+        answer_guess_to_int_while = answer_guess_to_int;
+
+        if (answer_guess_to_int == answer_to_int)
+        {
+            Console.WriteLine("You guessed it!");
+        }
+        else if (answer_guess_to_int > answer_to_int)
+        {
+            Console.WriteLine("Lower");
+        }
+        else if(answer_guess_to_int < answer_to_int)
+        {
+            Console.WriteLine("Higher");
+        }
+
+        }while(answer_to_int != answer_guess_to_int_while);
+
     }
 }
